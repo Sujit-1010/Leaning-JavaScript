c13cc981ff9c86e21791476354daac16a6fe5bc3
Sujit-1010 <gadesujit10@gmail.com>
2025-08-29 09:45:40 +0530

    Varaible Done

---
+++ basics/varaible.js
@@ -0,0 +1,23 @@
+const accountId = 123456
+let accountMail = "sujit@gmail.com"
+var accountPass = "12345"
+accountCity = "jaipur"
+let name ;
+
+//accountId = 5 // not allowed 
+
+accountMail = "abcd@gmail.com"
+accountPass = "7894"
+accountCity = "Barshi"
+
+console.table([accountId,accountMail,accountPass,accountCity,name])
+
+/*
+prefer not to use var 
+beacuse of issue in block and functional scope
+*/
+
+//use const and let
+
+
+
