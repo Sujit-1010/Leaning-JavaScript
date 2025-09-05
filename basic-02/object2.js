cbf7d82d10f130dc15a52715019536bb3efb67cd
Sujit <gadesujit10@gmail.com>
2025-09-05 09:45:55 +0530

    object lec 2

--- basic-02/object2.js
+++ basic-02/object2.js
@@ -1,5 +1,5 @@
-// const tinderUser = new Object()
-const tinderUser = {}
+// const tinderUser = new Object() --singleton object 
+const tinderUser = {} // non singleton object
 
 tinderUser.id = "123abc"
 tinderUser.name = "Sammy"
@@ -26,10 +26,10 @@ const obj4 = {5: "a", 6: "b"}
 // const obj3 = { obj1, obj2 }
 // const obj3 = Object.assign({}, obj1, obj2, obj4)
 
-const obj3 = {...obj1, ...obj2}
+const obj3 = {...obj1, ...obj2} //we use this to combine object
 // console.log(obj3);
 
-
+//when values come from database 
 const users = [
     {
         id: 1,
@@ -48,12 +48,13 @@ const users = [
 users[1].email
 // console.log(tinderUser);
 
-// console.log(Object.keys(tinderUser));
+// console.log(Object.keys(tinderUser)); --output datatype is array 
 // console.log(Object.values(tinderUser));
 // console.log(Object.entries(tinderUser));
 
 // console.log(tinderUser.hasOwnProperty('isLoggedIn'));
 
+//+++++++++++++++++++++++++++++++++++++++++++
 
 const course = {
     coursename: "js in hindi",
