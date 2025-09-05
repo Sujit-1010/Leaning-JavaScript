f6a158a655ef1188df47bcb284c3e81b14d99151
Sujit <gadesujit10@gmail.com>
2025-09-05 14:19:51 +0530

    IIFE done

--- basic-03/iife.js
+++ basic-03/iife.js
@@ -1,10 +1,25 @@
 // Immediately Invoked Function Expressions (IIFE)
 
+// to avoid global pollution we use fe or to remove pollution 
 
 (function chai(){
     // named IIFE
     console.log(`DB CONNECTED`);
 })();
+
+//if we cant end  above func with semi colon then below func give error  
+// (function chai(){
+//     // named IIFE
+//     console.log(`DB CONNECTED`);
+// })();
+
+
+//Arrow Function 
+
+( () => {
+    console.log(`DB CONNECTED TWO`);
+} )();
+
 
 ( (name) => {
     console.log(`DB CONNECTED TWO ${name}`);
